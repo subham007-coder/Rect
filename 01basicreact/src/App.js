abe162a280d659150ad2471e953bcbe79c123268
subham007-coder <dsubham109@gmail.com>
2023-12-23 20:38:23 +0530

    add

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,11 @@
+import Chai from './Chai'
+
 function App() {
   return (
-    <h1>Start With React 22/12/2023 | Subham Das</h1>
+    <>
+      <Chai />
+      <h1>Start With React 22/12/2023 | Subham Das</h1>
+    </>
   );
 }
 
