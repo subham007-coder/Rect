a35d61d02cf07fa0d478bd7881b604ddf35cda14
subham007-coder <dsubham109@gmail.com>
2023-12-25 12:24:39 +0530

    add

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+let [Counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    setCounter(Counter + 1)
+    if (Counter == 20) {
+      setCounter(Counter)
+    }
+  }
+  
+  const removeVal = () => {
+    setCounter(Counter - 1)
+    if (Counter == 0) {
+      setCounter(Counter)
+    }
+  }
+
+  return (
+    <>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {Counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={removeVal}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
