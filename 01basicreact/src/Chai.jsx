db5c8640110b8dcb7e00525025a58cf46af15df8
subham007-coder <dsubham109@gmail.com>
2024-02-26 10:29:42 +0530

    start with new

--- 01basicreact/src/Chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h1>Chai or react</h1>
-    )
-}
-
-export default Chai
--- 01vitereact/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h1>Chai Ready</h1>
-    )
-}
-
-export default Chai
