0c84aa8cbfdb4f81c9b598e1db1f0ff5341e8f48
subham007-coder <dsubham109@gmail.com>
2024-02-27 20:50:59 +0530

    make counter app in react useState hook

--- counterReact/src/App.jsx
+++ counterReact/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
+  let [counter, setCounter] = useState(15);
+
+  const addvalue = () => {
+    if (counter == 99) {
+      setCounter(counter);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (counter == 0) {
+      setCounter(counter);
+    } else {
+      setCounter(counter - 1);
+    }
+  };
 
   return (
     <>
-     <h1>Chai Or React</h1>
+      <h1>Chai Or React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addvalue}>Add value</button>
+      <br />
+      <br />
+      <button onClick={removeValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
