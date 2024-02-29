289e0d8ab62aad9e61f8fa6aec0d5de07eeba3a4
subham007-coder <dsubham109@gmail.com>
2024-02-29 14:00:55 +0530

    make bg changer in react

--- bgColor-Changer/src/App.jsx
+++ bgColor-Changer/src/App.jsx
@@ -2,38 +2,62 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Card from "./Card";
 
 function App() {
-  const [counter, setConter] = useState(10);
-
-  const addVal = () => {
-    if (counter === 20) {
-      setConter(counter);
-    } else {
-      setConter(counter + 1);
-    }
-  };
-
-  const removeVal = () => {
-    if (counter === 0) {
-      setConter(counter);
-    } else{
-      setConter(counter - 1);
-    }
-  };
+  const [color, setColor] = useState("olive");
 
   return (
-    <>
-      <button onClick={addVal}>Add Number</button>
-      <br />
-      <h1>{counter}</h1>
-      <br />
-      <button onClick={removeVal}>Remove Number</button>
-
-    <Card brandName="Redmi" mi="Mi" xomi="Xiaomi"/>
-
-    </>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+            className="px-3 outline-none rounded-full text-white py-3"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+          onClick={() => setColor("green")}
+            className="px-3 outline-none rounded-full text-white py-3"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+          onClick={() => setColor("blue")}
+            className="px-3 outline-none rounded-full text-white py-3"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+          onClick={() => setColor("purple")}
+            className="px-3 outline-none rounded-full text-white py-3"
+            style={{ backgroundColor: "purple" }}
+          >
+            Purple
+          </button>
+          <button
+          onClick={() => setColor("olive")}
+            className="px-3 outline-none rounded-full text-white py-3"
+            style={{ backgroundColor: "olive" }}
+          >
+            Olive
+          </button>
+          <button
+          onClick={() => setColor("pink")}
+            className="px-3 outline-none rounded-full text-black py-3"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 
