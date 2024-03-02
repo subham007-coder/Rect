b590012a8c00b416b9cfdef53b4761c770f495c7
subham007-coder <dsubham109@gmail.com>
2024-03-02 21:43:29 +0530

    start with react route

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Header from 
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
