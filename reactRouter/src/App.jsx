d4b42354ecd60da1b133a08e099bd734cdbbb2f8
subham007-coder <dsubham109@gmail.com>
2024-03-02 22:14:12 +0530

    home, about, header, footer done

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import "./App.css";
-import Header from 
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
+      
     </>
   );
 }
