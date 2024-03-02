bab0400adaed9150ebbed0c1cf8551d40e9d4eae
subham007-coder <dsubham109@gmail.com>
2024-03-02 13:29:07 +0530

    add in ui

--- carencyConverter/src/App.jsx
+++ carencyConverter/src/App.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import { InputBox } from "./components/InputBox";
+import useCurrencyInfo from "./hooks/useCurrencyinfo";
 import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
 
+  const CurrencyInfo = useCurrencyInfo(from);
   return (
     <>
       <h1 className="text-3xl bg-orange-500">Currency app</h1>
