5ff857c18c545aa4a1271ca033fcfacd36f3b13f
subham007-coder <dsubham109@gmail.com>
2024-03-03 21:09:41 +0530

    contact page

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
-import Contact from "./Contect/Contact.jsx";
+import Contact from "./components/Contect/Contact.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -31,14 +36,13 @@ import Contact from "./Contect/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="" element={<Home/>}/>
-      <Route path="about" element={<About/>}/>
-      <Route path="contact" element={<Contact/>}/>
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
     </Route>
   )
-)
-
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
